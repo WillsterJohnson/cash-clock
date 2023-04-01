6334357a71c46abc797d67a59eea8b8816c5a055
Will Johnson <willster@willsterjohnson.com>
2023-04-01 23:05:49 +0100

    :sparkles: app-wide settings button

--- routes/+layout.svelte
+++ routes/+layout.svelte
@@ -1,16 +1,45 @@
 <script lang="ts">
-  const agentHas = (keyword: string) =>
-    navigator.userAgent.toLowerCase().search(keyword.toLowerCase()) > -1;
-  const isFireFox = agentHas("Firefox") || agentHas("FxiOS") || agentHas("Focus");
+  import Settings from "$lib/settings.svelte";
+  import { slide } from "svelte/transition";
+
+  let settings = false;
 </script>
 
-<svelte:head>
-  <link
-    rel="stylesheet"
-    href="{window.location.protocol}//{window.location.host}/colors{isFireFox ? '' : '-hsl'}.css"
-  />
-</svelte:head>
+<button class="settings-toggle" on:click={() => (settings = !settings)}>
+  <span class="icon">{settings ? "arrow_back" : "settings"}</span>
+</button>
+
+<div class="settings-wrap">
+  {#if settings}
+    <div transition:slide>
+      <Settings />
+    </div>
+  {/if}
+</div>
+
 <slot />
 
 <style lang="scss">
+  .settings-toggle {
+    position: absolute;
+    border: none;
+    color: var(--foreground);
+    top: 7vmin;
+    left: 5vmin;
+    padding: 2vmin;
+    border-radius: 100vmin;
+    z-index: 200;
+    background: var(--background-elevated-50);
+    transition: 250ms ease;
+    cursor: pointer;
+    &:is(:hover, :focus-within) {
+      outline: none;
+      background: var(--background-elevated);
+    }
+  }
+
+  .settings-wrap {
+    position: relative;
+    z-index: 100;
+  }
 </style>
