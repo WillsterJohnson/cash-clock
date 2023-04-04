d01e170087f7c3d90ca5c96fdc834a961acd301c
Will Johnson <willster@willsterjohnson.com>
2023-04-04 12:24:24 +0100

    :sparkles: (bubblewrap): use bubblewrap to build for play store

---
+++ bubblewrapped/app/src/main/java/com/willsterjohnson/cash_clock/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package com.willsterjohnson.cash_clock.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
